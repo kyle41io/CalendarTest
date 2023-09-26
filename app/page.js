ad59df6faa50aeb96551ebbd2a4cd1eedcafa7bb
kyle41io <vankien41hust@gmail.com>
2023-09-26 08:54:21 +0700

    modify

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Calendar multiple={multiple} locale={locale} selectedDates={selectedDates} setSelectedDates={setSelectedDates} handleToggle={handleToggle} />
+      <Calendar multiple={multiple} selectedDates={selectedDates} setSelectedDates={setSelectedDates} handleToggle={handleToggle} />
       <SelectedDates selectedDates={selectedDates} locale={locale} />
     </main>
   );
